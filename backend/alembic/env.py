3bf7c4ac813c7ef17f34a5628512a18c8eeb6695
molhot <sakata19991214@gmail.com>
2026-03-20 16:04:16 +0900

    sys pathの追加関数の記載を修正

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,7 +4,14 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(
+    0,
+    os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(__file__)
+        )
+    )
+)
 
 from app.core.database import Base
 from app.core.config import settings
