28afc9d231ccef7277b09030e29bff12bb0a4c3b
molhot <sakata19991214@gmail.com>
2026-03-21 12:27:33 +0900

    primal keyにindexが不要なので修正

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -7,7 +7,7 @@ from app.core.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
 
     username = Column(
         String(50),
