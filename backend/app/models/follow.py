12da6cac118a28ccdd3a92126931ac18ebd2d5e7
molhot <sakata19991214@gmail.com>
2026-03-21 11:55:42 +0900

    followmodelを追加

---
+++ backend/app/models/follow.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timezone
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, UniqueConstraint
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+
+class Follow(Base):
+    __tablename__ = "follows"
+
+    __table_args__ = (
+        UniqueConstraint(
+            "follower_id",
+            "followed_id",
+            name="uq_follower_followed"
+        ),
+    )
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    follower_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        nullable=False
+    )
+
+    followed_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        nullable=False
+    )
+
+    created_at = Column(
+        DateTime(timezone=True),
+        default=lambda:datetime.now(timezone.utc)
+    )
+
+    follower = relationship(
+        "User",
+        foreign_keys=[follower_id],
+        back_populates="following"
+    )
+
+    followed = relationship(
+        "User",
+        foreign_keys=[followed_id],
+        back_populates="followers"
+    )
